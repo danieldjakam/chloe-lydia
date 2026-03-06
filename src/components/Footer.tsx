e9b3adc96fc2eaf50ac58a967cfb708906f4b65d
danieldjakam <danieldjakam@Dans-PC.local>
2026-03-06 16:13:11 +0100

    ok

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className="bg-black text-white/40 text-center py-7 font-nunito text-[0.82rem]">
       © 2026{' '}
-      <span className="text-rose">Daily Mysteries</span>{' '} by 
+      <span className="text-rose">Daily Mysteries</span>{' '} by {' '}
       <a href="https://www.danieldjakam.com" target="_blank" rel="noopener noreferrer" className="text-rose font-bold" style={{ textDecoration: 'underline' }}>Daniel Djakam</a>.
     </footer>
   )
